8016feecb21b3459086e8a8fc1e94e527faa794b
RasulPirsoltanov1 <resulresull510@gmail.com>
2022-10-31 19:05:10 +0400

    created and added flowchart documents

--- homework3/avarage sum.cs
+++ homework3/avarage sum.cs
@@ -1,10 +1,12 @@
 //Task - 1: 1 - 10 aralığında olan cüt ədədlərin ədədi ortasını tapan alqoritm yazın.
-int sum=0;
+int sum=0,counter=0;
 for(int i = 1; i < 10; i++)
 {
     if (i % 2 == 0)
     {
         sum = sum + i;
+        counter++;
     }
 }
-Console.WriteLine(sum);
+
+Console.WriteLine("ededi orta = "+sum/counter);
