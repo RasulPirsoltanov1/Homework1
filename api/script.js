b7930b992f55b27ba35f872b887210f773179ac2
RasulPirsoltanov1 <7m0uvuf@code.edu.az>
2022-11-12 20:30:19 +0400

    created api

--- api/script.js
+++ api/script.js
@@ -13,12 +13,11 @@ function get_data() {
                 const element = json[i];
 
                 card.innerHTML += `<div class="card rounded col-md-3 shadow-lg p-3 mb-5 bg-body rounded" style="width: 18rem; margin:20px; "><div class="card-body" > <h5 class="card-title"> title: ${json[i].title}</h5>
-            <h6 class="card-subtitle mb-2 text-muted"> User id: ${json[i].userId}</h6>
+            <h6 class="card-subtitle mb-2 text-muted"> User id: ${json[i]["userId"]}</h6>
             <p class="card-text">Card text: ${json[i].completed}</p>
             <a href="https://jsonplaceholder.typicode.com/todos/${json[i].id}" class="btn btn-primary card-text" style="height:40px;" target="_blank">Details : ${json[i].id}</p>
             </div>`;
             }
-            console.log(json[i])
         })
 }
 function hide_data() {
