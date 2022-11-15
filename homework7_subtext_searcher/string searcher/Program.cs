47a0ebbdb48542542b25dcc51f3df7b808bb0253
RasulPirsoltanov1 <7m0uvuf@code.edu.az>
2022-11-15 15:19:13 +0400

    subtext searher

---
+++ homework7_subtext_searcher/string searcher/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Input Text: ");
+string biggest = Console.ReadLine();
+Console.Write("Input Subtext: ");
+string smallest = Console.ReadLine();
+Console.WriteLine("**************************RESULT********************************");
+Console.WriteLine("");
+int counter=0;
+bool ok=true;
+for (int i = 0; i < biggest.Length; i++)
+{
+    for (int j = 0; j < smallest.Length; j++)
+    {
+        if ((i + j) < biggest.Length)
+        {
+            if (biggest[i + j] == smallest[j])
+            {
+                counter++;
+                if (counter == smallest.Length)
+                {
+                    Console.WriteLine("Subtext found. ");
+                    ok = false;
+                }
+            }
+            else
+            {
+                counter = 0;
+                break;
+            }
+        }
+    }
+}
+if(ok)
+{
+    Console.WriteLine("Subtext not found.");
+}
+
