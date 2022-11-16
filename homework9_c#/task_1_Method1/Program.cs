a444158085aa199fea0cbe94424828740f9497a7
RasulPirsoltanov1 <7m0uvuf@code.edu.az>
2022-11-16 19:45:32 +0400

    c# tasks

---
+++ homework9_c#/task_1_Method1/Program.cs
@@ -0,0 +1,35 @@
+﻿//1 - Bir method yazın parametr olaraq bir array və size parametrləri
+//qəbul etsin etsin həmin arrayin ölçüsünü göndərilən size-a bərabər etsin.
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        int[] currentArray = { 12, 32, 34, 3 };
+        
+        Console.WriteLine("*************************");
+        Console.WriteLine("first array length before : " + currentArray.Length);
+        Resize(ref currentArray, 2);
+        Console.WriteLine("first array length then : "+currentArray.Length);
+    }
+    public static void Resize(ref int[] array,int newSize)
+    {
+        int[] newArray = new int[newSize];
+        for (int i = 0; i < newSize; i++)
+        {
+            if (array.Length > i)
+            {
+                newArray[i] = array[i];
+            }
+            else
+            {
+                newArray[i] = 0;
+            }
+        }
+        array = newArray;
+
+    }
+
+
+}
