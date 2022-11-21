78c60746d3a50bf29c60c44329a9c2f7a7f4cdea
RasulPirsoltanov1 <7m0uvuf@code.edu.az>
2022-11-21 13:39:31 +0400

    das

---
+++ c#/Classroom task2/Classroom task2/Program.cs
@@ -0,0 +1,37 @@
+﻿getReverse("za fd");
+
+void getReverse(string text)
+{
+    char[] reverse = new char[text.Length];
+    string result = String.Empty;
+    string result2 = String.Empty;
+    string word = String.Empty;
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i] != ' ' || i == text.Length - 1)
+        {
+            word+= text[i];
+        }
+        if (text[i] == ' ' || i == text.Length - 1)
+        {
+            result2 = result2+textreturner(word);
+            result = string.Empty;
+        }
+
+    }
+    Console.WriteLine(result);
+
+}
+
+
+string textreturner(string text)
+{
+    string result = String.Empty;
+    for (int j = text.Length; j >=0; j--)
+    {
+        result+=text[j];
+        
+    }
+    return result;
+
+}
---
+++ c#/StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine(StringBuilder2("Hellolll", 'l', 'd', 1, 3));
+string StringBuilder2(string text, char old, char newchar, int start, int count)
+{
+    string word = string.Empty;
+    for (int i = 0; i < text.Length; i++)
+    {
+
+        if (text[i] != old )
+        {
+            word += text[i];
+        }
+        if (i > start)
+        {
+            if (start + count > i)
+            {
+                if (text[i] == old)
+                {
+                    word += newchar;
+                }
+            }
+        }
+
+
+    }
+    return word;
+}
