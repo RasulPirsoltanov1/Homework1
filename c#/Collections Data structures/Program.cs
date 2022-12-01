bdabd625beddd72e472073bd642ebce1e2fdf09a
RasulPirsoltanov1 <7m0uvuf@code.edu.az>
2022-12-01 22:28:45 +0400

    homework 9

---
+++ c#/Collections Data structures/Program.cs
@@ -0,0 +1,53 @@
+﻿#region  Dictionary
+
+//Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+//keyValuePairs.Add("Rasul", 21);
+//keyValuePairs.Add("Flankes", 41);
+//keyValuePairs.Add("besmakes", 31);
+//keyValuePairs.Add("xalxin usagi", 32);
+
+////foreach (var pair in keyValuePairs)
+////{
+////    Console.WriteLine($"{pair.Key} {pair.Value}");
+////}
+//Console.WriteLine(keyValuePairs["Rasul"]);
+
+
+//Console.Write("enter key ");
+
+//string keyName = Console.ReadLine();
+//if (keyValuePairs.ContainsKey(keyName))
+//{
+//    Console.WriteLine(keyValuePairs[keyName]);
+//}
+//else
+//{
+//    Console.WriteLine("does not exists the value for this key!");
+//}
+#endregion
+
+#region Queue
+
+//Queue<int> newQueue = new Queue<int>();
+//newQueue.Enqueue(1);
+//newQueue.Enqueue(2);
+//newQueue.Enqueue(3);
+//newQueue.Enqueue(4);
+//Console.WriteLine(newQueue.Count);
+//int countQueue = newQueue.Count;
+//for (int i = 0; i < countQueue; i++)
+//{
+//    Console.WriteLine(newQueue.Dequeue());
+//}
+
+#endregion
+#region HashSet
+HashSet<string> keys = new HashSet<string>();
+keys.Add("Salam");
+keys.Add("Salam1");
+HashSet<string> keys2 = new HashSet<string>();
+keys2.Add("Salam");
+keys2.Add("Salam2");
+keys.IntersectWith(keys2);
+Console.ReadLine();
+#endregion
---
+++ c#/List exists/Program.cs
@@ -0,0 +1,87 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        MyList<int> myList = new MyList<int>();
+        myList.Add(1);
+        myList.Add(112);
+        myList.Add(211);
+        for (int i = 0; i < myList.Count; i++)
+        {
+            Console.WriteLine(myList[i]);
+        }
+        MyList<string> stringList = new MyList<string>();
+        stringList.Add("bir");
+        stringList.Add("iki");
+        stringList.Add("uc");
+        stringList.Add("uc");
+        Console.WriteLine("******String******");
+        stringList[2] = "dort";
+        for (int i = 0; i < stringList.Count; i++)
+        {
+            Console.WriteLine(stringList[i]) ;
+        }
+        Console.WriteLine(stringList.Exists("ucw"));
+        stringList[2] = "dort";
+        Console.WriteLine(stringList[2]);
+    }
+}
+class MyList<T>
+{
+    private T[] arr;
+    public int Capacity { get; set; }
+    public int Count { get; set; }
+    public int Length { get; set; }
+    public MyList()
+    {
+        Capacity = 0;
+        arr = new T[Capacity];
+    }
+    public void Add(T a)
+    {
+        if (Capacity == 0)
+        {
+            Capacity = Capacity + 1;
+            Array.Resize(ref arr, Capacity);
+            arr[Capacity - 1] = a;
+        }
+        else if (Capacity <= arr.Length)
+        {
+            Capacity = arr.Length + 1;
+            Array.Resize(ref arr, Capacity);
+            arr[Capacity - 1] = a;
+        }
+        Count++;
+    }
+    public bool Exists(T element)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (checker(arr[i], element))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public bool checker(object a, object b)
+    {
+        if (a == b)
+        {
+            return true;
+        }
+        return false;
+    }
+    public T this[int index]
+    {
+        get
+        {
+            return arr[index];
+        }
+        set
+        {
+            arr[index] = value;
+        }
+    }
+}
+
