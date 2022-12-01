384b01491309d33bd0622bec303fc108d44bd7cb
[Ayatcode] <[2mwkd2k@code.edu.az]>
2022-12-01 12:58:50 +0400

    bsdj

---
+++ c#/Mini Project/Models/Status.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Project.Models
+{
+    internal class Status
+    {
+        //-Id
+        //- Title
+        //- Content
+        //- SharedDate - DateTime tipindən bir property, statusdan yeni bir obyekt yaranan zaman yarandığı tarix set olunacaq.
+        //- GetStatusInfo() -geriyə "Title: test title - Content: test content - shared 5 seconds ago"(burda qeyd olunan "shared
+        //5 seconds ago" hissəsi dinamik olmalıdı status neçə saniyə əvvəl paylaşılıbsa o yazılmalıdı) bu tipdə bir məlumat qaytaracaq.
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime SharedDate { get; set; } = DateTime.Now;
+        public Status(string title,string content)
+        {
+            Content= content;
+            Title= title;
+            Id++;
+        }
+        public string GetStatusInfo()
+        {
+            return $"Title: {Title} - Content: {Content} - {(DateTime.Now.Ticks - SharedDate.Ticks)/10000000} seconds ago";
+        }
+    }
+}
---
+++ c#/Mini Project/Models/User.cs
@@ -0,0 +1,65 @@
+﻿using Mini_Project.Expections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Project.Models
+{
+    internal class User
+    {
+        //User class
+        //- Id
+        //- Statuses - status obyektlərini özündə saxlayan bir list olacaq.
+        //- Username
+        //- ShareStatus() - parametr olaraq bir status obyekti qəbul edib statuses listinə əlavə edəcək.
+        public static int userid;
+        private int Id;
+        private Status[] Statuses;
+        public string Username { get; set; }
+        public User(string username)
+        {
+            userid++;
+            Username = username;
+            Statuses = new Status[0];
+        }
+        public void ShareStatus(Status status)
+        {
+            Array.Resize(ref Statuses, Id + 1);
+            Statuses[Id] = status;
+            Id++;
+        }
+        public void GetStatus()
+        {
+            foreach (var stat in Statuses)
+            {
+                Console.WriteLine(Username + stat.GetStatusInfo());
+            }
+        }
+        //GetStatusById() - parametr olaraq nullable int tipindən bir id qəbul edəcək həmin id-li
+        //statusu tapıb geriyə qaytaracaq.
+        public Status GetStatusById(int id)
+        {
+            return Statuses[id];
+        }
+        public Status[] GetAllStatuses()
+        {
+            return Statuses;
+        }
+        public string FilterStatusByDate(DateTime time)
+        {
+            bool chechk = true;
+            for (int i = 0; i < Statuses.Length; i++)
+            {
+                if (time < Statuses[i].SharedDate)
+                {
+                    chechk = false;
+                    return $"{Statuses[i].SharedDate}";
+                }
+            }
+            throw new NotFoundException("Data not found!");
+        }
+    }
+}
