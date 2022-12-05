fea6aa66956a2416edbbd24141416f681a508ee6
[Ayatcode] <[2mwkd2k@code.edu.az]>
2022-12-05 11:17:06 +0400

    nfdj

---
+++ c#/1_c# practica/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        area test = new area();
+        test.calcarea(new circle());
+        test.calcarea(new rectangle());
+    }
+}
+
+
+public class area
+{
+    public void calcarea(Ifigure type)
+    {
+        type.calc();
+    }
+}
+
+
+public class circle : Ifigure
+{
+    public void calc()
+    {
+        Console.WriteLine("circle");
+    }
+}
+
+public interface Ifigure
+{
+    public void calc();
+
+}
+public interface IfigureSum
+{
+    public void figureSum();
+
+}
+public class rectangle : Ifigure, IfigureSum
+{
+    public void calc()
+    {
+        Console.WriteLine("rectangle");
+    }
+
+    public void figureSum()
+    {
+        throw new NotImplementedException();
+    }
+}
